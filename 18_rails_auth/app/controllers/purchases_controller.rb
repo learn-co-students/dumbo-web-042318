76c7d19735d4ab21d522becc48d74ce888dbab68
Tim Campbell <timothylevicampbell@gmail.com>
2018-05-31 11:28:00 -0400

    Add Rails Auth code

---
+++ 18_rails_auth/app/controllers/purchases_controller.rb
@@ -0,0 +1,30 @@
+class PurchasesController < ApplicationController
+  def new
+    @purchase = Purchase.new
+    @brews = Brew.all
+  end
+
+  def create
+    #/purchases
+    @purchase = Purchase.new(purchase_params)
+
+    if  @purchase.valid? # save
+      @purchase.save
+      redirect_to purchase_path(@purchase.id)
+      # redirect_to purchase_path(@purchase)
+      # redirect_to @purchase
+    else
+      @brews = Brew.all
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:customer_name, :brew_id)
+  end
+end
