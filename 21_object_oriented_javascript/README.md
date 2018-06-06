ac419691df3bea3f8d9ff444420218d17573850c
Tim Campbell <timothylevicampbell@gmail.com>
2018-06-06 11:03:57 -0400

    Add oo-js

---
+++ 21_object_oriented_javascript/README.md
@@ -0,0 +1,14 @@
+# Object-Oriented JS
+
+## Objectives
+* Create objects as object literals or by instantiating them using new Object
+* Add properties to store values (both primitive and non-primitive) on objects
+* Write functions that use this and assign them as properties on objects
+* Identify the differences between bind, call, and apply
+* Use arrow functions to automatically bind context
+* Create a class using both ES5 and ES6 syntaxes. In both syntaxes, students should be able to:
+* Write a constructor
+* Instantiate a new object
+* Create private "class" variables using the ES6 class syntax, closures, and IIFEs
+* Write instance methods for JS classes
+* Write class methods for JS classes (e.g. filters and search functions)
---
+++ 21_object_oriented_javascript/index.js
@@ -0,0 +1,100 @@
+// bind, call, apply
+// design patterns: factory
+
+// const author = {
+//    "firstName": "first",
+//    "lastName": "last"
+// }
+
+// const book1 = {
+//   "title": "Something",
+//   "author": author,
+//   "printBook": printBook
+// }
+
+// const book2 = {
+//   "title": "anotherBook",
+//   "author": author,
+//   "printBook": printBook
+// }
+
+
+// function bookFactory(title /* STRING */, author /* STRING */) {
+//   // what needs to happen here to create a new book?
+//   const book = {}
+//
+//   book.title = title
+//   book.author = { name: author }
+//   book.printBook = bookFactory.printBook
+//   book.printAuthor = bookFactory.printAuthor
+//   book.class = bookFactory.class
+//
+//   return book
+// }
+//
+// bookFactory.class = "Book"
+//
+// bookFactory.printBook = function printBook() {
+//   console.log(this.title)
+// }
+//
+// bookFactory.printAuthor = function printAuthor() {
+//   console.log(this.author.name)
+// }
+//
+// const book1 = bookFactory("The Yellow Wallpaper", "Charlotte Perkins Gilman")
+// const book2 = bookFactory("The Bell Jar", "Sylvia Plath")
+
+// creates a new object {}
+// sets the prototype of that object to the constructor's prototype
+// call the constructor function WITH THE OBJECT AS THE VALUE OF THIS
+// return the new object
+// function Book(title, author) {
+//   this.title = title
+//   this.author = { name: author }
+//   this.constructor.all.push(this)
+// }
+//
+// // Book.prototype.class = "Book"
+// Book.class = "Book"
+// Book.all = []
+// Book.printAllBooks = function() {
+//   console.log(this.all)
+// }
+// Book.prototype.printBook = function printBook() {
+//   console.log(this.title)
+// }
+// Book.prototype.printAuthor = function printAuthor() {
+//   console.log(this.author.name)
+// }
+//
+// const book1 = new Book("The Yellow Wallpaper", "Charlotte Perkins Gilman")
+// const book2 = new Book("The Bell Jar", "Sylvia Plath")
+
+
+// class Book {
+//   constructor(title, author) {
+//     this.title = title
+//     this.author = { name: author }
+//     this.constructor.all.push(this)
+//   }
+//
+//   printBook() {
+//     console.log(this.title)
+//   }
+//
+//   printAuthor() {
+//     console.log(this.author.name)
+//   }
+//
+//   static printAllBooks() {
+//     // class method
+//     console.log(this.all)
+//   }
+// }
+//
+// Book.all = []
+//
+//
+// const book1 = new Book("The Yellow Wallpaper", "Charlotte Perkins Gilman")
+// const book2 = new Book("The Bell Jar", "Sylvia Plath")
