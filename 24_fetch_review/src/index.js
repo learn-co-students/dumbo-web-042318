09b6caa479b5d1f4cf9c739ba627ba71ea649b0d
perpepajn25 <perpepajn25@gmail.com>
2018-06-12 12:45:24 -0400

    mid review

--- 24_fetch_review/src/index.js
+++ 24_fetch_review/src/index.js
@@ -1,14 +1,38 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   const choreListEl = document.querySelector('#chore-list');
+  const choreForm = document.getElementById("new-chore-form");
+  choreForm.addEventListener('submit', postChore)
+  choreListEl.addEventListener('click', (e) => {
+    if (e.target.className === 'delete-button'){
+      Adapter.deleteOne(e.target.parentElement.dataset.id).then(console.log)
+      // remove the correct chore card from the page - where should this be done?
+    }
+  })
+
+  function renderChore(chore) {
+    choreListEl.innerHTML += `
+      <div class='chore-card' data-id=${chore.id}>
+        <button class='delete-button'> x </button>
+        <h3>${chore.title}</h3>
+        <p>${chore.priority}</p>
+        <p>${chore.duration}</p>
+      </div>
+    `;
+  }
+
+  function postChore(e) {
+    e.preventDefault()
+    let data = {
+      title: e.target.title.value,
+      priority: e.target.priority.value,
+      duration: e.target.duration.value
+    }
+    Adapter.createOne(data).then(renderChore)
+  }
+
   Adapter.getAll().then((chores /* array */) => {
-    chores.forEach(function(chore) {
-      choreListEl.innerHTML += `
-        <div>
-          <h3>${chore.title}</h3>
-          <p>${chore.priority}</p>
-          <p>${chore.duration}</p>
-        </div>
-      `;
-    });
+    console.log(chores)
+    chores.forEach(renderChore);
   });
 });
