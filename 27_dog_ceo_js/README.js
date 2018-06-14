0bb22fad5f5a457af61a3bf5f318bd8a664a073a
Tim Campbell <timothylevicampbell@gmail.com>
2018-06-13 20:43:45 -0400

    Add Readme with deliverables

---
+++ 27_dog_ceo_js/README.js
@@ -0,0 +1,6 @@
+DOGCEO
+* User should be able to see a list of all dogs
+* User should be able to delete dogs from the list
+* User should be able to refresh a dog image in the list
+* User should be able to sort dog breed list alphabetically
+* User should be able to sort dog breed list by length of dog breed
