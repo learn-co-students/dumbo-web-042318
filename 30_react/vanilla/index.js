543c91859f46680124c650334399389e9c1d837a
Tim Campbell <timothylevicampbell@gmail.com>
2018-06-25 13:25:48 -0400

    Add react starter code

---
+++ 30_react/vanilla/index.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(React.createElement('h1'), document.getElementById('app'));
+/* ReactDOM.render(<h1 />, document.getElementById('app')); */
+
+// ReactDOM.render(
+//   React.createElement('h1', {}, 'hello'),
+//   document.getElementById('app')
+// );
+//
+/* ReactDOM.render(<h1>Hello</h1>, document.getElementById('app')); */
